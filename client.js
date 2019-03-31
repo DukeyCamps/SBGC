4957013a7749568152bb3f6c5ca544f39d284d48
Duke <31015852+DukeyCamps@users.noreply.github.com>
2019-03-31 20:16:40 +0000

    +Added functionality
    __init__ remade

--- client.js
+++ client.js
@@ -1,47 +1,198 @@
 //@Dooki
-document.getElementById('everything');
+
+function __main__()
+{
+  colorChanger()
+  dayChange()
+  dateChange()
+  timeChange()
+  scheduleChange()
+  loopit()
+}
+function scheduleChange(){
+  scheduler(14, 00, 14, 45, "class", 1)
+  scheduler(14, 45, 14, 50, "break", 99)
+  scheduler(14, 50, 15, 35, "class", 2)
+  scheduler(15, 35, 15, 50, "break", 99)
+  scheduler(15, 50, 16, 35, "class", 3)
+  scheduler(16, 35, 16, 40, "break", 99)
+  scheduler(16, 40, 17, 25, "class", 4)
+  scheduler(17, 25, 17, 35, "break", 99)
+  scheduler(17, 35, 18, 20, "class", 5)
+  scheduler(18, 20, 18, 25, "break", 99)
+  scheduler(18, 25, 19, 10, "class", 6)
+  scheduler(19, 10, 19, 15, "break", 99)
+  scheduler(19, 15, 20, 00, "class", 7)
+}
+
+function loopit()
+{
+  var t = setTimeout(__main__, 500);
+}
 
 
+function colorChanger(hexcolor='#000000')
+{
+var divs = ["DAY_B", "DATE_B", "CurrentTime_B", "TN_B"]
+var m;
+for(m = 0; m < divs.length; m++){
+var x = document.getElementsByClassName(divs[m]);
+var i;
+for (i = 0; i < x.length; i++) {
+  x[i].style.backgroundColor = hexcolor;
+}
+}
 
-function timeChange(){
+}
+//scheduler(10, 45, 11, 30, class, 2)
+//drugi cas od 10:45 do 11:30
+//redni 99 default znaci da je odmor
+function scheduler(start_hr, start_mn, end_hr, end_mn, mode, redni=99)
+{
   var today = new Date();
   var hr = today.getHours();
   var mn = today.getMinutes();
   var sc = today.getSeconds();
-  var day = today.getDay();
+
+  nowTime = hr * 3600 + mn * 60 + sc
+  startTime = start_hr * 3600 + start_mn * 60
+  endTime = end_hr * 3600 + end_mn * 60
+  var leftTime;
+  var leftMn;
+  var leftSc;
+  if(startTime <= nowTime && nowTime <= endTime)//PAY ATTENTION
+  {
+    leftTime = endTime - nowTime // -1
+    leftMn = Math.floor(leftTime / 60)
+    leftSc = leftTime - leftMn * 60
+    document.getElementById("TIMELEFT").innerHTML = checkTime(leftMn) + ":"+ checkTime(leftSc)
+    SWITCHMODE(mode, redni)
+  }
+  else
+  {
+    //document.getElementById("TIMELEFT").innerHTML = "?"
+    //document.getElementById("TIMEEVENT").innerHTML = ""
+  }
+
+}
 
 
+
+
+
+
+
+
+
+function timeChange()
+{
+  //gets the time from the today class
+  var today = new Date();
+  var hr = today.getHours();
+  var mn = today.getMinutes();
+  var sc = today.getSeconds();
+
+
+  //sets the time to the 00:00:00 format
   hr = checkTime(hr)
   mn = checkTime(mn)
   sc = checkTime(sc)
-  dayC = checkDay(day)
 
+
+  //finds the id of the text and changes it to the current time
   document.getElementById('hr').innerHTML = hr;
   document.getElementById('mn').innerHTML = mn;
   document.getElementById('sc').innerHTML = sc;
-  document.getElementById('dayL').innerHTML = dayC;
-  var t = setTimeout(timeChange, 500);
-  console.log("changed time")
+  console.log("Regulated Time.")
 }
 
-function checkTime(el_id){
 
-  if(10 > el_id){
+//a display format function
+function checkTime(el_id)
+{
+  //Checks whether the time is in the xx:xx:xx format and automatically corrects it
+  if(10 > el_id)
+  {
       el_id = "0"+el_id.toString();
-                }
+  }
   return el_id;
-}
-function checkDay(day){
-switch (day) {
-  case 1:return "ponedeljak";break;
-  case 2:return "utorak";break;
-  case 3:return "sreda";break;
-  case 4:return "cetvrtak";break;
-  case 5:return "petak";break;
-  case 6:return "subota";break;
-  case 7:return "nedelja";break;
-  default:return "SBG";break;
 
 }
 
+
+//assigns a day to a day of the week value
+function checkDay(day)
+{
+  switch (day)
+  {
+    case 1:return "ponedeljak";break;
+    case 2:return "utorak";break;
+    case 3:return "sreda";break;
+    case 4:return "cetvrtak";break;
+    case 5:return "petak";break;
+    case 6:return "subota";break;
+    case 7:return "nedelja";break;
+    default:return "SBG";break;
+  }
 }
+
+
+
+
+
+//a function that changes the date in the upper right corner
+function dateChange()
+{
+  var today = new Date()
+  var dd = today.getDate()
+  var mm = today.getMonth()
+  var yyyy = today.getFullYear()
+  currentDate =  dd + '/' + mm + '/' + yyyy;
+  document.getElementById('dt').innerHTML = currentDate
+}
+
+
+function dayChange()
+{
+  var today = new Date();
+  var day = today.getDay();
+  dayC = checkDay(day)
+  document.getElementById('dayL').innerHTML = dayC;
+}
+
+
+
+
+function SWITCHMODE(mode, redni)
+{
+  if (mode == "class")
+  {
+    colorChanger('#e74c3c')
+    document.getElementById("TIMEEVENT").innerHTML = "Do kraja " + redni.toString() + ". casa"
+  }
+  else if(mode == "break")
+  {
+    document.getElementById("TIMEEVENT").innerHTML = "Do kraja odmora"
+    colorChanger('#27ae60')
+  }
+
+
+
+
+}
+function SHOWCLOCK()
+{
+  document.getElementById('everything').style.visibility = 'visible'
+}
+
+
+function HIDECLOCK()
+{
+  document.getElementById('everything').style.visibility = 'hidden'
+}
+
+//TO BE IMPLEMENTED:
+/*
+//use color #c82333 as primary RED
+
+*/
